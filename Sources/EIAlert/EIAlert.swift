10e060e6d611bd7f73cea9e23c4ab7e20361a286
Emil Ismayilov <emilismayilov095@gmail.com>
2024-02-17 20:37:28 +0400

    Add alerts

--- Sources/EIAlert/EIAlert.swift
+++ Sources/EIAlert/EIAlert.swift
@@ -1,2 +1,31 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import UIKit
+
+public class EIAlert {
+    public static let shared = EIAlert()
+    private init() {}
+    
+    public let rootViewController = UIApplication.shared.firstKeyWindow?.rootViewController
+}
+
+
+public extension EIAlert {
+    func showAlertWith(title: String, message: String, buttonTitle: String = "OK") {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        rootViewController?.present(alert, animated: true, completion: nil)
+    }
+    
+    func showAlertWithCompletionAndCancel(title: String, message: String, buttonTitle: String, cancelTitle: String = "Cancel", completion: @escaping ()->Void ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: buttonTitle, style: .destructive) { (_) in
+            completion()
+        }
+        let cancel = UIAlertAction(title: cancelTitle, style: .cancel)
+        
+        alert.addAction(action)
+        alert.addAction(cancel)
+        rootViewController?.present(alert, animated: true, completion: nil)
+    }
+}
